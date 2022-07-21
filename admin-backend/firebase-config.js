41383632505d39ec987ac50e729be3c6d8342299
OB Hettiarachchi <osanda.buwaneka@gmail.com>
2022-07-21 18:39:46 +0530

    Firebase connect

---
+++ admin-backend/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDxaheONobOJ7Ca_R0uTEsK6LFPPLp__N8",
+    authDomain: "salesman-bot-56ef5.firebaseapp.com",
+    projectId: "salesman-bot-56ef5",
+    storageBucket: "salesman-bot-56ef5.appspot.com",
+    messagingSenderId: "351167086438",
+    appId: "1:351167086438:web:e770f3dce9aa32eb8452a7",
+    measurementId: "G-854TQ2TFS9"
+};
+
+const app = initializeApp(firebaseConfig);
