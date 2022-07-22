27fd0e111bde240e43a7eb0d38eb4613b6a802b3
OB Hettiarachchi <osanda.buwaneka@gmail.com>
2022-07-22 23:09:08 +0530

    Frontend init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -9,13 +8,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer">
         </a>
       </header>
     </div>
