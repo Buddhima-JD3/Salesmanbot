420b06167d7fb254bf212f6f9469d3c431a91595
Buddhima <it20071998@my.sliit.lk>
2022-08-11 03:19:08 +0530

    Config Changes

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Webstorm\Salesmanbot2\env
+set VIRTUAL_ENV=C:\Users\Lenovo\OneDrive\Documents\GitHub\Salesmanbot\env
 
 if not defined PROMPT set PROMPT=$P$G
 
