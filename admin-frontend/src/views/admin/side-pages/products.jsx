a96b3917d64514ed5d8720f3275a4d2ad02132de
ThevinRahu <thevinvr@gmail.com>
2022-08-12 08:32:35 +0530

    admin backend connection

---
+++ admin-frontend/src/views/admin/side-pages/products.jsx
@@ -0,0 +1,216 @@
+import React, { Component } from "react";
+import Container from 'react-bootstrap/Container';
+import axios from "axios";
+import { withRouter } from 'react-router-dom';
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+
+class products extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            searchKey: "",
+            customer: [
+                {
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    phone: "",
+                    age: "",
+                    _id: "",
+                },
+            ],
+    
+    
+        };
+      }
+      
+      componentDidMount() {
+        this.retrieveCustomers();
+      }
+    
+      retrieveCustomers() {
+        axios.get("http://localhost:8080/getallproducts").then((res) => {
+          // if (res.data.success) {
+            this.setState({
+              customer: res.data,
+            });
+          
+          console.log(res.data);
+        });
+      }
+    
+      onDelete = (id) => {
+    
+        if (window.confirm("Are you sure you wish to delete this user?")) {
+          axios.delete(`http://localhost:8080/deleteproduct/${id}`).then((res) => {
+            alert("Deleted Successfully");
+            this.retrieveCustomers();
+    
+          });
+        }
+      };
+    
+      filterData(customer, searchKey) {
+        const result = customer.filter(
+          (customer) =>
+            customer.brand.toLowerCase().includes(searchKey) ||
+            customer.healthStat.toLowerCase().includes(searchKey) ||
+            customer.ingreduents.toLowerCase().includes(searchKey) ||
+            customer.item_type.toLowerCase().includes(searchKey) ||
+            customer.nutrition.toLowerCase().includes(searchKey)
+        );
+        this.setState({ items: result });
+      }
+    
+      handleSearchArea = (e) => {
+        const searchKey = e.currentTarget.value;
+    
+        axios.get("http://localhost:8080/getallproducts").then((res) => {
+          if (res.data.success) {
+            this.filterData(res.data.existingItems, searchKey);
+          }
+        });
+      };
+       
+      render() {
+
+        return (
+    
+            <div class="container" >
+              <br></br>
+              <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{border:"25rem"}}>
+        <Container>
+          <Navbar.Brand href="#home">Admin Panel</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/admin">Admin Panel</Nav.Link>
+              <NavDropdown title="Dashboard Items" id="collasible-nav-dropdown">
+                {/* <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item> */}
+                <NavDropdown.Item href="customers">
+                  Customers
+                </NavDropdown.Item>
+                <NavDropdown.Item href="itemsold">
+                  Item Sold
+                </NavDropdown.Item>
+                <NavDropdown.Item href="orders">
+                  Orders
+                </NavDropdown.Item>
+                <NavDropdown.Item href="products">
+                  Products
+                </NavDropdown.Item>
+                <NavDropdown.Item href="users">
+                  Users
+                </NavDropdown.Item>
+  
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="admin">
+                  Admin Panel
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+  
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            
+              <div class="card login-card">
+                <div className="container">
+          <br/>
+            <div className="row">
+              <div className="col-lg-9 mt-2 mb-2">
+                
+                <h3><b>Product Details Dashboard</b></h3>
+              </div>
+              <div className="col-lg-3 mt-2 mb-2">
+                <input
+                  className="form-control"
+                  type="search"
+                  placeholder="Search"
+                  name="searchQuery"
+                  onChange={this.handleSearchArea}></input>
+              </div>
+            </div>
+            <table className="table table-hover" style={{ marginTop: "40px" }}>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Phone</th>
+                  <th scope="col">Age</th>
+                  <th scope="col">Gender</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.customer.map((customer, index) => (
+  
+                  <tr key={index}>
+                    <th scope="row">{index + 1}</th>
+                    <td>
+                      <a
+                        href={`localhost:8090/api/customer/${customer._id}`}
+                        style={{ textDecoration: "none" }}>
+                        {customer.name}
+                      </a>
+                    </td>
+                    <td>{customer.email}</td>
+                    <td>{customer.phone}</td>
+                    <td>{customer.age}</td>
+                    <td>{customer.gender}</td>
+                    <td>
+                      <a className="btn btn-warning" href={`/editCustomer/${customer._id}`}>
+                        <i className="fas fa-edit"></i>&nbsp;Edit
+                      </a>
+                      &nbsp;
+                      <button
+                        className="btn btn-danger" type=""
+                        href= {`/customer/delete/${customer._id}`}
+                        onClick={() => this.onDelete(customer._id)}>
+                        <i className="far fa-trash-alt"></i>&nbsp;Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+              <br/>
+              <button className="btn btn-success"><a href="customer/add" style={{textDecoration:'none',color:'white'}}>Add New Product</a></button>  
+              <button className="btn btn-secondary" style={{marginLeft:"10px" }}><a href="/report" style={{textDecoration:'none',color:'white'}}>Generate Product Report</a></button>
+              <button className="btn btn-dark" style={{marginLeft:"10px" }}><a href="/purchaseOrder" style={{textDecoration:'none',color:'white'}}>Generate Product Purchase Report</a></button>
+          </div>
+                
+              </div>
+            </div>
+      
+  
+    )
+  }
+        
+      }
+  
+
+export default products;
---
+++ admin-frontend/src/views/owner/home/addProduct.jsx
@@ -0,0 +1,132 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class AddProduct extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            brand: "",
+            healthStat: "",
+            ingreduents: "",
+            item_type:"",
+            nutrition:""
+        };
+      }
+    
+      handleInputChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+          ...this.state,
+          [name]: value,
+        });
+      };
+    
+      onSubmit = (e) => {
+        e.preventDefault();
+    
+        const { brand, healthStat, ingreduents, item_type,nutrition } = this.state;
+    
+        const data = {
+            brand:brand,
+            healthStat: healthStat,
+            ingreduents: ingreduents,
+            item_type:item_type,
+            nutrition:nutrition
+        };
+    
+        console.log(data);
+        axios.post("http://localhost:8080/addproduct", data).then((res) => {
+          if (res.data.success) {
+            this.setState({
+                brand: "",
+                healthStat: "",
+                ingreduents: "",
+                item_type:"",
+                nutrition:""
+            });
+          }
+        });
+      };
+      render() {
+        return (
+          <div className="col-md-8 mt-4 mx-auto">
+            <br></br>
+            <form>
+              <div className="form-group h3 mb-3 font-weight-normal">
+                <label for="topic">Brand</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="brand"
+                  name="brand"
+                  placeholder="Brand"
+                  value={this.state.brand}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="form-group h3 mb-3 font-weight-normal">
+                <label for="description">Health Stat</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="healthStat"
+                  name="healthStat"
+                  placeholder="Health Stat"
+                  value={this.state.healthStat}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="form-group h3 mb-3 font-weight-normal">
+                <label for="postCategory">Ingredients</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="ingreduents"
+                  name="ingreduents"
+                  placeholder="Ingredients"
+                  value={this.state.ingreduents}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="form-group h3 mb-3 font-weight-normal">
+                <label for="price">Item Type</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="item_type"
+                  name="item_type"
+                  placeholder="Item Type"
+                  value={this.state.item_type}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="form-group h3 mb-3 font-weight-normal">
+                <label for="price">Nutrition</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="nutrition"
+                  name="nutrition"
+                  placeholder="Nutrition"
+                  value={this.state.nutrition}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <br></br>
+              <div class="col-sm-10">
+                <button
+                  className="btn btn-success"
+                  type="submit"
+                  style={{ marginTop: "15px" }}
+                  onClick={this.onSubmit}
+                >
+                  <i className="fas fa-save"></i>&nbsp;Save
+                </button>
+              </div>
+            </form>
+          </div>
+        );
+      }
+};
+
