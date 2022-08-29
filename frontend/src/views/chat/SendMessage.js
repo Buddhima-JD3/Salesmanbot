6102d131ace404a8c0ab41660c10c40b2407532b
Buddhima <it20071998@my.sliit.lk>
2022-08-29 14:04:37 +0530

    Chat interface added

---
+++ frontend/src/views/chat/SendMessage.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { db, auth } from '../../firebase'
+import firebase from 'firebase/compat/app';
+import { Input, Button } from '@material-ui/core'
+import Speechtotext from "../speech-to-text/speechtotext";
+import TranscribeOutput from "../speech-to-text/utils/TranscribeOutput";
+
+export default function SendMessage({ scroll }) {
+    const [msg, setMsg] = useState('')
+
+    async function sendMessage(e) {
+        e.preventDefault()
+        const { uid, photoURL } = auth.currentUser
+
+
+        await db.collection('messages').add({
+            text: msg,
+            photoURL,
+            uid,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        setMsg('')
+        scroll.current.scrollIntoView({ behavior: 'smooth' })
+    }
+    return (
+        <div>
+            <form onSubmit={sendMessage}>
+
+                <div className="sendMsg">
+                    <Input style={{ width: '78%', fontSize: '15px', fontWeight: '550', marginLeft: '5px', marginBottom: '-3px' }} placeholder='Message...' type="text" value={msg} onChange={e => setMsg(e.target.value)} />
+                    <Button style={{ width: '18%', fontSize: '15px', fontWeight: '550', maxWidth: '200px'}} type="submit">Send</Button>
+                    <Speechtotext></Speechtotext>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+
--- frontend/src/views/speech-to-text/speechtotext.jsx
+++ frontend/src/views/speech-to-text/speechtotext.jsx
@@ -4,23 +4,28 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import withStyles from "@material-ui/core/styles/withStyles";
 import Typography from "@material-ui/core/Typography";
-
+import chat from "../chat/Chat";
 import "bootstrap/dist/css/bootstrap.min.css";
+import SendMessage from '../chat/SendMessage'
 
 import speechToTextUtils from "./utils/utility_transcribe";
 import TranscribeOutput from "./utils/TranscribeOutput";
 import SettingsSections from "./utils/SettingsSection";
+import {Input} from "@material-ui/core";
 
 const useStyles = () => ({
     root: {
+
+        // borderTop: '1px solid lightgray',
         display: 'flex',
         textAlign: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-end',
         alignItems: 'center',
         margin: 'auto',
+        backgroundColor: '#fafafa !important',
     },
     title: {
-        marginBottom: '20px',
+        // marginBottom: '20px',
     },
     settingsSection: {
         marginBottom: '20px',
@@ -91,6 +96,7 @@ const App = ({classes}) => {
 
     function onStop() {
         setIsRecording(false)
+        console.log({transcribedData});
         flushInterimData() // A safety net if Google's Speech API doesn't work as expected, i.e. always sends the final result
         speechToTextUtils.stopRecording();
     }
@@ -99,20 +105,19 @@ const App = ({classes}) => {
         <div class="flex-container" className={classes.root}>
             <div class="flex-child magenta" className={classes.buttonsSection}>
                 {!isRecording && <Button onClick={onStart} variant="primary" style={{
-                    borderRadius: '56%', margin: '24px', padding: '12px', height: '56px',
+                    borderRadius: '56%', margin: '7px', padding: '12px', height: '56px',
                     width: '55px'
                 }}><i className="fa-solid fa-microphone fa-lg"></i></Button>}
                 {isRecording && <Button onClick={onStop} variant="danger" style={{
-                    borderRadius: '56%', margin: '24px', padding: '12px', height: '56px',
+                    borderRadius: '56%', margin: '7px', padding: '12px', height: '56px',
                     width: '55px'
                 }}><i className="fa-solid fa-microphone fa-lg"></i></Button>}
             </div>
 
             <div>
-                <div class="flex-child green" className={classes.speech}>
+                <Input placeholder='Message...' type="text" value={transcribedData}   onChange={e => setMsg(e.target.value)} />
                     <TranscribeOutput transcribedText={transcribedData}
                                       interimTranscribedText={interimTranscribedData}/>
-                </div>
             </div>
         </div>
     );
