ee6e70598f6d0be520a3c8b93483964b96cad760
Buddhima <it20071998@my.sliit.lk>
2022-09-23 05:56:28 +0530

    negotiatoon process updated

--- backend/negotiate.py
+++ backend/negotiate.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from create_data import *
 from ontology_lookup import *
+import random
 
 cred = credentials.Certificate("D:\Downloads\salesman-bot-56ef5-firebase-adminsdk-ozj39-4ef8a3c068.json")
 # firebase_admin.initialize_app(cred)
@@ -27,16 +28,22 @@ db = firestore.client()
 def getAllProducts():
     docs =  db.collection('products').get()
     dicts = []
+    productNameList = []
     for doc in docs:
         # Prints all the products in the collection
         # print(doc.to_dict())
         dicts.append(doc.to_dict())
         # print(dicts[0]["location"], dicts[0]["quantity"])
+
+    # Get all the Products to a List
+    for i in dicts:
+        productNameList = i["productName"] + " " + i["brand"]
+        print(productNameList)
     return
 
 
 # Queries for a particular product that has been recognized from the chatbot and returns the length of the result
-def checkNluResult(text):
+def productAvailability(text):
     docs = list(db.collection('products').where("productName", "==", text).get())
 #   print("# of documents in collection: {}".format(len(docs)))
     return len(docs)
@@ -49,9 +56,10 @@ def getProductDetails(text):
     for doc in docs1:
         dicts.append(doc.to_dict())
         try:
-            result = (dicts[0]["location"], dicts[0]["quantity"], dicts[0]["category"] )
+            result = (dicts[0]["location"], dicts[0]["quantity"], dicts[0]["category"])
         except:
-            print("Something else went wrong")
+            result = 0
+            print("Something else went wrong or No Available Products!!!")
     response = getResponse(result)
     return(response)
 
@@ -60,34 +68,73 @@ def getProductDetails(text):
 def getFromPurchaseHistory(text):
     print("Getting Products From ML " + text)
     result = "History Based Products"
-    response = getResponse(result)
+    list = ['Non-Fat Milk', 'Biscuits', 'IceCream']
+    availableProducts = []
+    for x in list:
+        if(productAvailability(x) > 0):
+            availableProducts.append(x)
+
+    if(len(availableProducts) != 0):
+        response = getResponse(result, availableProducts)
+    else:
+        print("No Products Available")
+        response = "notok"
     return(response)
+
 
 # Get related to cateogories of products to the original product
 def getSimilarProductsCluster(text):
     print("Getting From Ontology " + text)
     result = "Ontology Based Products"
-    response = getResponse(result)
+    list = ['Non-Fat Milk', 'Biscuits', 'IceCream']
+    availableProducts = []
+    for x in list:
+        if (productAvailability(x) > 0):
+            availableProducts.append(x)
+
+    if (len(availableProducts) != 0):
+        response = getResponse(result, availableProducts)
+    else:
+        print("No Products Available")
+        response = "notok"
     return (response)
+
 
 # Get products to be recommended based on weather
 def getProductsWeather(text):
     print("Getting Weather info " + text)
     result = "Weather Based Products"
-    response = getResponse(result)
+    list = ['Biscuits', 'IceCream']
+    availableProducts = []
+    for x in list:
+        if (productAvailability(x) > 0):
+            availableProducts.append(x)
+
+    if(len(availableProducts) != 0):
+        response = getResponse(result, availableProducts)
+    else:
+        print("No Products Available")
+        response = "notok"
     return (response)
 
-# Gets the response from the chatbot when products are offered
-def getResponse(result):
-    print("Getting Response from Rasa")
-    print("Sending Result " + str(result))
-    return str("notok")
 
-#Start of Negotiation Process
+# Gets the response from the chatbot when products are offered
+def getResponse(result, productList):
+    print("Sending Result " + str(result))
+    print(productList)
+    print("Getting Response from Rasa")
+    response = ["ok", "notok"]
+    if(response[random.choice([0,1])] == "ok"):
+        getProductDetails('Non-Fat Milk')
+        return str("ok")
+    else:
+        return str("notok")
+
+# Negotiation Process
 def main():
     getAllProducts()
     text = 'Full Cream Milk'
-    if (checkNluResult(text) > 0 ):
+    if (productAvailability(text) > 0 ):
         result = getProductDetails(text)
         print(result)
         print('Product Available\n')
