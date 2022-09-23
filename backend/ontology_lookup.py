f3a289cb90b9e33c717af3deca1a3acb2b3669f7
ThevinRahu <thevinvr@gmail.com>
2022-09-23 09:56:56 +0530

    sparql and ontology quering for negotiation

--- backend/ontology_lookup.py
+++ backend/ontology_lookup.py
@@ -3,6 +3,7 @@ from owlready2 import *
 onto = get_ontology("./ontology/DairyProducts.owl")
 onto.load()
 print("Ontology Successfully Loaded")
+#<http://example.com/DairyProductsOntology/#>
 
 print("Total Classes : ", list(default_world.sparql("""
                  SELECT (COUNT(?x) AS ?nb)
@@ -10,5 +11,50 @@ print("Total Classes : ", list(default_world.sparql("""
           """)))
 
 
-print(list(onto.classes()))
+a = list(default_world.sparql("""
+                 SELECT ?subject ?object
+                 	WHERE { ?subject rdfs:subClassOf ?object }
+          """))
 
+print(a)
+
+
+def categoryBased(category):
+
+    categoryBasedList = list(default_world.sparql("""
+                     SELECT
+                     	?search
+                     	(STR(?label) AS ?name)
+                     	(count(?class) as ?total)
+                     	WHERE {
+                     	values ?search {
+                            {category}
+                     	}
+                     	{?class rdfs:subClassOf+ ?search}
+                     	OPTIONAL {?search rdfs:label ?label.}
+                     }
+                     GROUP BY ?search ?label
+              """))
+
+    return categoryBasedList
+
+
+
+def weatherBased(weather):
+
+    weatherBasedList = list(default_world.sparql("""
+                     SELECT
+                     	?search
+                     	(STR(?label) AS ?name)
+                     	(count(?class) as ?total)
+                     	WHERE {
+                     	values ?search {
+                            {weather}
+                     	}
+                     	{?class rdfs:subClassOf+ ?search}
+                     	OPTIONAL {?search rdfs:label ?label.}
+                     }
+                     GROUP BY ?search ?label
+              """))
+
+    return weatherBasedList
