2701b09d8ec8f4c91c82db46c030dbdfe021529b
Thevin Rahubedda <thevinvr@gmail.com>
2022-10-11 13:21:49 +0530

    add / edit products frontend and backend work done

--- admin-frontend/src/apis/modules/products.js
+++ admin-frontend/src/apis/modules/products.js
@@ -4,5 +4,8 @@ import axios from "../axios";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     retrieveProducts: () => axios.get(`/api/getallproducts`),
-    deleteProduct: (id) => axios.delete(`/api/deleteproduct/${id}`)
+    retrieveEditProduct: (idp) => axios.get(`/api/getproduct/${idp}`),
+    deleteProduct: (id) => axios.delete(`/api/deleteproduct/${id}`),
+    updateProduct: (idp,data) => axios.put(`/api/updateproduct/${idp}`,data),
+    addProduct: (data) => axios.post(`/api/addproduct`,data)
 }
