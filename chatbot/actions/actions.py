8fc636283dd63151abffda9da137753402906421
OB Hettiarachchi <osanda.buwaneka@gmail.com>
2022-10-13 02:02:24 +0530

    fix

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,9 +1,16 @@
-from typing import Any, Text, Dict, List
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
 
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-from ...backend import negotiate
 
 class ActionHelloWorld(Action):
     def name(self) -> Text:
@@ -24,10 +31,10 @@ class ActionGetProduct(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        b = negotiate.getAllProducts()
-        a = "aaaa"
-        print(b)
-        dispatcher.utter_message(text="product list : " + a)
+
+        a = " | test | "
+        b = "mapped data"
+        dispatcher.utter_message(text="product list : " + a + b)
 
         return []
 
@@ -39,21 +46,7 @@ class ActionSearchproduct(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        entities = tracker.latest_message['entities']
-        print(entities)
-
-        for e in entities:
-            if e['entity'] == 'milk power':
-                name = e['value']
-
-            if name == "Highland":
-                message = "finding highland products"
-
-            if name == "Milo":
-                message = "finding Milo products"
-
-        dispatcher.utter_message(message)
+        dispatcher.utter_message(text="Search product is running")
 
         return []
 
